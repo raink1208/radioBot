79c4676f8abd6b21f18173c5439cf0e5f4590235
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-04-03 14:40:20 +0900

    インタラクション方法を変更

--- src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
@@ -5,17 +5,16 @@ import com.github.raink1208.radiobot.interaction.selection.PlayPlaylist
 import com.github.raink1208.radiobot.interaction.selection.SelectMenuInteractionBase
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent
-import net.dv8tion.jda.api.interactions.components.ComponentInteraction
 
 object InteractionHandler {
     private val selectMenuInteractions = mutableMapOf<String, SelectMenuInteractionBase>("play_playlist" to PlayPlaylist)
     private val buttonInteractions = mutableMapOf<String, ButtonInteractionBase>()
 
-    fun interact(event: ComponentInteraction) {
-        if (event is SelectMenuInteractionEvent) {
-            selectMenuInteractions[event.selectMenu.id]?.interact(event)
-        } else if (event is ButtonInteractionEvent) {
-            buttonInteractions[event.button.id]?.interact(event)
-        }
+    fun interact(event: SelectMenuInteractionEvent) {
+        selectMenuInteractions[event.selectMenu.id]?.interact(event)
+    }
+
+    fun interact(event: ButtonInteractionEvent) {
+        buttonInteractions[event.button.id]?.interact(event)
     }
 }
