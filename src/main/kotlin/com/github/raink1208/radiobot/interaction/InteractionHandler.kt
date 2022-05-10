2bc8360dab85daf6f8efb958f0ced1f4ed73aa4f
rain1208 <58714901+raink1208@users.noreply.github.com>
2022-05-10 09:30:14 +0900

    actionの登録 解除 取得を追加

--- src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
@@ -1,26 +1,17 @@
 package com.github.raink1208.radiobot.interaction
 
-import com.github.raink1208.radiobot.interaction.button.ButtonInteractionBase
-import com.github.raink1208.radiobot.interaction.button.DeleteCheckButton
-import com.github.raink1208.radiobot.interaction.selection.DeletePlaylist
-import com.github.raink1208.radiobot.interaction.selection.PlayPlaylist
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
-import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent
-
 class InteractionHandler {
-    private val selectMenuInteractions = mutableMapOf(
-        "play_playlist" to PlayPlaylist,
-        "delete_playlist" to DeletePlaylist
-    )
-    private val buttonInteractions = mutableMapOf<String, ButtonInteractionBase>(
-        "delete_check" to DeleteCheckButton
-    )
+    private val interactionActions = HashMap<Long, InteractionAction>()
 
-    fun interact(event: SelectMenuInteractionEvent) {
-        selectMenuInteractions[event.selectMenu.id]?.interact(event)
+    fun register(id: Long, action: InteractionAction) {
+        interactionActions[id] = action
     }
 
-    fun interact(event: ButtonInteractionEvent) {
-        buttonInteractions[event.button.id]?.interact(event)
+    fun unregister(id: Long) {
+        interactionActions.remove(id)
+    }
+
+    fun getInteractionAction(id: Long): InteractionAction? {
+        return interactionActions[id]
     }
 }
