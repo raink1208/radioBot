e3a3cc33fc70bb417d626debbb3ba5de5be8a054
rain1208 <58714901+raink1208@users.noreply.github.com>
2022-05-10 13:14:58 +0900

    Interaction周りのリファクタリング

--- src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/interaction/InteractionHandler.kt
@@ -1,6 +1,8 @@
 package com.github.raink1208.radiobot.interaction
 
-class InteractionHandler {
+import net.dv8tion.jda.api.interactions.components.ComponentInteraction
+
+object InteractionHandler {
     private val interactionActions = HashMap<Long, InteractionAction>()
 
     fun register(id: Long, action: InteractionAction) {
@@ -13,5 +15,14 @@ class InteractionHandler {
 
     fun getInteractionAction(id: Long): InteractionAction? {
         return interactionActions[id]
+    }
+
+    fun interact(event: ComponentInteraction) {
+        val action = getInteractionAction(event.message.idLong)
+        if (action == null) {
+            event.reply("有効期限切れです").queue()
+            return
+        }
+        action.interact(event)
     }
 }
--- src/main/kotlin/com/github/raink1208/radiobot/interaction/button/DeleteCheckButton.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/interaction/button/DeleteCheckButton.kt
@@ -1,17 +1,21 @@
 package com.github.raink1208.radiobot.interaction.button
 
+import com.github.raink1208.radiobot.interaction.InteractionAction
+import com.github.raink1208.radiobot.interaction.InteractionHandler
+import com.github.raink1208.radiobot.model.Playlist
 import com.github.raink1208.radiobot.service.PlaylistService
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import net.dv8tion.jda.api.interactions.components.ComponentInteraction
 
-object DeleteCheckButton: ButtonInteractionBase {
-    override val buttonId = "delete_check"
-
-    override fun interact(event: ButtonInteractionEvent) {
-        val playlist = PlaylistService.getPlaylist(event.message.contentRaw)
-        if (playlist == null) {
-            event.reply("playlist: ${event.message.contentRaw} が見つかりませんでした").queue()
-            return
+class DeleteCheckButton(private val playlist: Playlist): InteractionAction {
+    override fun interact(event: ComponentInteraction) {
+        if (event is ButtonInteractionEvent) {
+            interact(event)
         }
+        InteractionHandler.unregister(event.message.idLong)
+    }
+
+    private fun interact(event: ButtonInteractionEvent) {
         if (playlist.author != event.user.idLong) {
             event.reply("プレイリストの削除は作成した人しかできません").queue()
             return
