9c92e7ca5b604346ed189135344eb439da97d45f
rain1208 <58714901+raink1208@users.noreply.github.com>
2022-05-11 13:11:18 +0900

    ページの取得メソッドの追加

--- src/main/kotlin/com/github/raink1208/radiobot/model/PageEmbed.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/model/PageEmbed.kt
@@ -1,9 +1,36 @@
 package com.github.raink1208.radiobot.model
 
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.MessageEmbed.Field
 
 class PageEmbed private constructor(val title: String, fields: List<Field>, separateCount: Int){
     private val chunkedFields = fields.chunked(separateCount)
+    var page: Int = 0
+
+    fun getNext(): MessageEmbed {
+        page++
+        return getEmbed(page)
+    }
+
+    fun getPrev(): MessageEmbed {
+        page--
+        return getEmbed(page)
+    }
+
+    fun getEmbed(page: Int): MessageEmbed {
+        val fields = getFields(page)
+        val builder = EmbedBuilder()
+        builder.setTitle(title)
+        for (field in fields) {
+            builder.addField(field)
+        }
+        return builder.build()
+    }
+
+    fun getFields(page: Int): List<Field> {
+        return chunkedFields[page]
+    }
 
     class PageEmbedBuilder {
         var title = ""
