c1764d472a56e90cdbae2957e31659eda9bcf650
rain1208 <58714901+raink1208@users.noreply.github.com>
2022-05-11 13:52:58 +0900

    PageEmbedControllerを追加

---
+++ src/main/kotlin/com/github/raink1208/radiobot/interaction/pageembed/PageEmbedController.kt
@@ -0,0 +1,38 @@
+package com.github.raink1208.radiobot.interaction.pageembed
+
+import com.github.raink1208.radiobot.interaction.InteractionAction
+import com.github.raink1208.radiobot.model.PageEmbed
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import net.dv8tion.jda.api.interactions.components.ComponentInteraction
+import net.dv8tion.jda.api.interactions.components.ItemComponent
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+
+class PageEmbedController(private val pageEmbed: PageEmbed, val embedMessageId: Long): InteractionAction {
+    companion object {
+        fun createActionRows(): List<ItemComponent> {
+            return listOf(
+                Button.primary("prev", "◀︎"),
+                Button.primary("next", "▶︎")
+            )
+        }
+    }
+
+    override fun interact(event: ComponentInteraction) {
+        if (event is ButtonInteractionEvent) {
+            when (event.componentId) {
+                "prev" -> prev(event)
+                "next" -> next(event)
+            }
+        }
+    }
+
+    fun prev(event: ButtonInteractionEvent) {
+        event.deferEdit().queue()
+        event.hook.editMessageEmbedsById(embedMessageId, pageEmbed.getPrev()).queue()
+    }
+
+    fun next(event: ButtonInteractionEvent) {
+        event.deferEdit().queue()
+        event.hook.editMessageEmbedsById(embedMessageId, pageEmbed.getNext()).queue()
+    }
+}
