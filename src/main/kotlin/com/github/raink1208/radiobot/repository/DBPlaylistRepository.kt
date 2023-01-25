327c37d73bb5fe94c063da32cd3c6c3a76bc9fe2
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-01-25 12:45:07 +0900

    プレイリストが存在するか確認するメソッドを実装

--- src/main/kotlin/com/github/raink1208/radiobot/repository/DBPlaylistRepository.kt
+++ src/main/kotlin/com/github/raink1208/radiobot/repository/DBPlaylistRepository.kt
@@ -4,6 +4,7 @@ import com.github.raink1208.radiobot.model.Playlist
 import com.github.raink1208.radiobot.util.Config
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.ResultSet
 
 class DBPlaylistRepository: IPlaylistRepository {
     private val connection: Connection = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPass())
@@ -21,7 +22,12 @@ class DBPlaylistRepository: IPlaylistRepository {
     }
 
     override fun existsPlaylist(playlistName: String): Boolean {
-        TODO("Not yet implemented")
+        val stmt = connection.prepareStatement("SELECT * FROM playlist WHERE name = ? LIMIT 1",
+            ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY)
+        stmt.setString(1, playlistName)
+        val result = stmt.executeQuery()
+        result.last()
+        return result.row != 0
     }
 
     override fun find(playlistName: String): Playlist? {
